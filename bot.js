fa5a204ef0c0a3cd790fccb7bb1f7942e6782bf4
Abdoamr893 <61624830+Abdoamr893@users.noreply.github.com>
2020-03-19 22:11:41 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("690284260388634665")
+setInterval(function() {
+channel.send(`aaaa`);
+}, 30)
+})
+
+client.login(Njg5NDM1OTA0NTAyMjY3OTcz.XnC2aQ.vc0w83G6LzMLUzpD9__RHLK9pHk);
